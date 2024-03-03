566a630d79f11dc956a38714ef08bb85e0ed1092
Junaid <junaidmahmud@iut-dhaka.edu>
2024-03-03 22:07:34 +0000

    Create maximum-odd-binary-number.py
    Daily Problem March 1, 2024

---
+++ Daily Challenge/maximum-odd-binary-number.py
@@ -0,0 +1,8 @@
+# Problem Link: https://leetcode.com/problems/maximum-odd-binary-number
+# March 1, 2024
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        hashTable = Counter(list(s))
+
+        return '1' * (hashTable['1'] - 1) + '0' * hashTable['0'] + '1' 
