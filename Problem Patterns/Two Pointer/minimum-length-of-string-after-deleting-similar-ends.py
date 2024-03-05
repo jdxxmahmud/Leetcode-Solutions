f56ea896f36bf9631c731a38557ef91972ed96b2
Junaid <junaidmahmud@iut-dhaka.edu>
2024-03-05 00:41:39 +0000

    Two Pointer - Medium
    minimum-length-of-string-after-deleting-similar-ends

---
+++ Problem Patterns/Two Pointer/minimum-length-of-string-after-deleting-similar-ends.py
@@ -0,0 +1,20 @@
+# Problem Link: https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        lo, hi = 0, len(s) - 1
+
+        while lo < hi and s[lo] == s[hi]:
+            currChar = s[lo]
+            
+            while lo <= hi:
+                if s[lo] != currChar:
+                    break
+                lo += 1
+
+            while lo <= hi:
+                if s[hi] != currChar:
+                    break
+                hi -= 1
+        
+        return hi  - (lo - 1)
