3b0a176c98b4139f6a222360784e5882221ce69a
Junaid <junaidmahmud@iut-dhaka.edu>
2024-03-08 01:22:58 +0000

    Array - Easy - count-elements-with-maximum-frequency

---
+++ Problem Patterns/Array/count-elements-with-maximum-frequency.py
@@ -0,0 +1,23 @@
+# Problem Link: https://leetcode.com/problems/count-elements-with-maximum-frequency
+
+class Solution:
+
+    def get_freq_tab(self, nums: List[int]) -> dict:
+        hashTab = {}
+        for num in nums:
+            if num in hashTab:
+                hashTab[num] += 1
+            else:
+                hashTab[num] = 1
+
+        return hashTab
+
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+
+        freq_tab = self.get_freq_tab(nums)
+        freq_freq = self.get_freq_tab(freq_tab.values())
+
+        max_freq = max(freq_freq.keys())
+
+        return freq_freq[max_freq] * max_freq
+         
