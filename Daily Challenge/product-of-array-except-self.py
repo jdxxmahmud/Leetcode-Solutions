c8d2444607af7c355808280ed87e05f43736e059
Junaid <junaidmahmud@iut-dhaka.edu>
2024-03-15 14:55:07 +0000

    Daily Question - product-of-array-except-self - Medium

---
+++ Daily Challenge/product-of-array-except-self.py
@@ -0,0 +1,30 @@
+# Problem Link: https://leetcode.com/problems/product-of-array-except-self
+# March 15, 2024
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod = 1
+        zerCount = 0
+        for num in nums:
+            if num != 0:
+                prod *= num
+            else:
+                zerCount += 1
+
+        res = []
+
+        for num in nums:
+            if num == 0:
+                if zerCount == 1:
+                    res.append(prod)
+                else:
+                    res.append(0)
+            else:
+                if zerCount != 0:
+                    res.append(0)
+                else:
+                    res.append(prod // num)
+
+        return res
+
+        
