82f8fdd535b53f1fa06ae0e5adf403fe9d75a57b
Junaid <junaidmahmud@iut-dhaka.edu>
2024-04-01 18:21:51 +0100

    length-of-last-word  - String - easy

---
+++ Problem Patterns/String/length-of-last-word.py
@@ -0,0 +1,17 @@
+# Problem Link: https://leetcode.com/problems/length-of-last-word
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        notSpace = False
+        length = 0
+
+        for i in s[::-1]:
+            if i != " ":
+                notSpace = True
+                length += 1
+            elif i == " " and notSpace:
+                break
+            elif i != " ":
+                length += 1
+        
+        return length
