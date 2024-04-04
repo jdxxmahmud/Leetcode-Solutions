f1cc10a799771a2b30bfc6cbc6b9148db3d85d6e
Junaid <junaidmahmud@iut-dhaka.edu>
2024-04-04 01:32:51 +0100

    maximum-nesting-depth-of-the-parentheses - Stack - Easy

---
+++ Problem Patterns/Stack/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,18 @@
+# Problem Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth = 0
+        q = []
+        curr = 0
+        for char in s:
+            if char == '(':
+                q.append(char)
+                curr += 1
+                depth = max(curr, depth)
+            elif char == ")":
+                q.pop()
+                curr -= 1
+
+            # print(char, q, curr, depth)
+
+        return depth
