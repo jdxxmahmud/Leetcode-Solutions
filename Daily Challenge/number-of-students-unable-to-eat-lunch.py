a941eb93c0d9c656ce495b62232a4ca40d7ea90c
Junaid <junaidmahmud@iut-dhaka.edu>
2024-04-09 01:20:21 +0100

    number-of-students-unable-to-eat-lunch - DQ - Apr 9, 2024

---
+++ Daily Challenge/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,18 @@
+# Problem Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+# April 9, 2024
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        stHash = defaultdict(int)
+        for i in students:
+            stHash[i] += 1
+        st = len(students)
+
+        for sd in sandwiches:
+            if stHash[sd]:
+                st -= 1
+                stHash[sd] -= 1
+            else:
+                return st
+
+        return st
