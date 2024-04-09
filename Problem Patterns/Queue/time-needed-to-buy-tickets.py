683a7032bb5f16cf9bc1db6daacad5842052d438
Junaid <junaidmahmud@iut-dhaka.edu>
2024-04-09 01:42:48 +0100

    time-needed-to-buy-tickets - Queue - Easy

---
+++ Problem Patterns/Queue/time-needed-to-buy-tickets.py
@@ -0,0 +1,17 @@
+# Problem Link: https://leetcode.com/problems/time-needed-to-buy-tickets
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        target = tickets[k]
+
+        res = 0
+
+        for i, ticket in enumerate(tickets):
+            if i <= k:
+                res += min(target, ticket)
+            else:
+                res += min(target - 1, ticket)
+
+            # print(ticket, res)
+            
+        return res
