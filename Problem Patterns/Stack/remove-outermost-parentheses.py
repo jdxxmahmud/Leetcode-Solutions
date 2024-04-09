61af7d2d3a70809efe10fbf7b4653f31c9da0a27
Junaid <junaidmahmud@iut-dhaka.edu>
2024-04-10 00:42:24 +0100

    remove-outermost-parentheses

---
+++ Problem Patterns/Stack/remove-outermost-parentheses.py
@@ -0,0 +1,25 @@
+# Problem Link: https://leetcode.com/problems/remove-outermost-parentheses/
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        outParen = False
+        Inner = 0
+        res = ""
+
+        for brac in s:
+            if outParen == False:
+                if brac == "(":
+                    outParen = True
+            else:
+                if brac == "(":
+                    Inner += 1
+                    res += brac
+                else:
+                    if Inner > 0:
+                        Inner -= 1
+                        res += brac
+                    else:
+                        outParen = False
+            # print(f"brac: {brac}, outParen: {outParen}, Inner: {Inner}, res: {res}")
+
+        return res
