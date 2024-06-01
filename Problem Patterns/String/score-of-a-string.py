bec8e41c49a2da9555c67ed7ad24b87fa9a9b8dc
Junaid <junaidmahmud@iut-dhaka.edu>
2024-06-01 01:51:40 +0100

    score-of-a-string - String - Easy

---
+++ Problem Patterns/String/score-of-a-string.py
@@ -0,0 +1,15 @@
+# Problem Link: https://leetcode.com/problems/score-of-a-string
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        res = 0
+
+        prevCharVal = int(ord(s[0]))
+        
+        for char in s:
+            charVal = int(ord(char))
+            res += abs(charVal - prevCharVal)
+
+            prevCharVal = charVal
+
+        return res
