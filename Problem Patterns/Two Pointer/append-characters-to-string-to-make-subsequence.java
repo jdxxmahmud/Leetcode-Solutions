93176cdb5639687be1e6c2f603591c64a003bae3
Junaid <junaidmahmud@iut-dhaka.edu>
2024-06-04 00:01:03 +0100

    append-characters-to-string-to-make-subsequence - Two Pointer - Medium

---
+++ Problem Patterns/Two Pointer/append-characters-to-string-to-make-subsequence.java
@@ -0,0 +1,21 @@
+# Problem Link: https://leetcode.com/problems/append-characters-to-string-to-make-subsequence
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length())
+        {
+            if (s.charAt(i) == t.charAt(j))
+            {
+                j++;
+            }
+
+            i++;
+
+        }
+
+        return t.length() - j;
+    }
+}
